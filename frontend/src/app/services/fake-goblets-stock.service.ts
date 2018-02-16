48b33db6724d926fe89e1badf3e4d497dd0bf316
Zozoleclown <Zozoleclown1@gmail.com>
2018-02-16 19:29:02 +0100

    Debut du fakdeService des gobelets

--- frontend/src/app/services/fake-goblets-stock.service.ts
+++ frontend/src/app/services/fake-goblets-stock.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { GobletsStock } from '../models/GobletsStock';
 
 @Injectable()
 export class FakeGobletsStockService {
 
   constructor() { }
+
+  getGobletsStocks(): Observable<Array<GobletsStock>> {
+    let gobletsStock: Array<GobletsStock> = [
+      {
+        createdAt: "2018-02-01 20:00:00",
+        value: "12"
+      }
+    ]
+  }
 
 }
