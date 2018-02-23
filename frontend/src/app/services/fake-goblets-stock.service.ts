8ce687d4561d8dfa1c42e28bf66f8b9ec0604ffd
Zozoleclown <Zozoleclown1@gmail.com>
2018-02-23 06:29:14 +0100

    Ajout de données dans les services

--- frontend/src/app/services/fake-goblets-stock.service.ts
+++ frontend/src/app/services/fake-goblets-stock.service.ts
@@ -10,6 +10,38 @@ export class FakeGobletsStockService implements GobletsStockServiceInterface {
   getGobletsStocks(): Observable<Array<GobletsStock>> {
     let gobletsStock: Array<GobletsStock> = [
       {
+        createdAt: "2018-02-01 18:00:00",
+        value: 25
+      },
+      {
+        createdAt: "2018-02-01 18:15:00",
+        value: 23
+      },
+      {
+        createdAt: "2018-02-01 18:30:00",
+        value: 18
+      },
+      {
+        createdAt: "2018-02-01 18:45:00",
+        value: 15
+      },
+      {
+        createdAt: "2018-02-01 19:00:00",
+        value: 12
+      },
+      {
+        createdAt: "2018-02-01 19:15:00",
+        value: 12
+      },
+      {
+        createdAt: "2018-02-01 19:30:00",
+        value: 12
+      },
+      {
+        createdAt: "2018-02-01 19:45:00",
+        value: 12
+      },
+      {
         createdAt: "2018-02-01 20:00:00",
         value: 12
       },
--- frontend/src/app/services/fake-water-level.service.ts
+++ frontend/src/app/services/fake-water-level.service.ts
@@ -10,30 +10,62 @@ export class FakeWaterLevelService implements WaterLevelServiceInterface {
 
     getWaterLevels(): Observable<WaterLevel[]> {
         let waterLevels = [
-            {
-                createdAt: "2018-02-01 20:00:00",
-                value: 0
-            },
-            {
-                createdAt: "2018-02-01 20:15:00",
-                value: 0
-            },
-            {
-                createdAt: "2018-02-01 20:30:00",
-                value: 0
-            },
-            {
-                createdAt: "2018-02-01 20:45:00",
-                value: 1.5
-            },
-            {
-                createdAt: "2018-02-01 21:00:00",
-                value: 1.5
-            },
-            {
-                createdAt: "2018-02-01 21:15:00",
-                value: 2
-            }
+          {
+            createdAt: "2018-02-01 18:00:00",
+            value: 0
+          },
+          {
+            createdAt: "2018-02-01 18:15:00",
+            value: 0.2
+          },
+          {
+            createdAt: "2018-02-01 18:30:00",
+            value: 0.3
+          },
+          {
+            createdAt: "2018-02-01 18:45:00",
+            value: 0.5
+          },
+          {
+            createdAt: "2018-02-01 19:00:00",
+            value: 0.6
+          },
+          {
+            createdAt: "2018-02-01 19:15:00",
+            value: 0.6
+          },
+          {
+            createdAt: "2018-02-01 19:30:00",
+            value: 0.6
+          },
+          {
+            createdAt: "2018-02-01 19:45:00",
+            value: 0.6
+          },
+          {
+            createdAt: "2018-02-01 20:00:00",
+            value: 0.6
+          },
+          {
+            createdAt: "2018-02-01 20:15:00",
+            value: 0.6
+          },
+          {
+            createdAt: "2018-02-01 20:30:00",
+            value: 0.6
+          },
+          {
+            createdAt: "2018-02-01 20:45:00",
+            value: 1
+          },
+          {
+            createdAt: "2018-02-01 21:00:00",
+            value: 1
+          },
+          {
+            createdAt: "2018-02-01 21:15:00",
+            value: 1.2
+          }
         ];
 
         return new Observable(sub => {
